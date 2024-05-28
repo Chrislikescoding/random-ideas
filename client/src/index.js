44c285ec1fdeb88d4639ea7f7a07b5b3aa389faf
ChrisLikesCoding <sharpchr@gmail.com>
2024-05-28 20:49:41 +0100

    Big update because I forgot!

---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+// import "./css/style.css";
+// import Modal from "./components/Modal";
+// import IdeaForm from "./components/IdeaForm";
+// import "@fortawesome/fontawesome-free/css/all.css";
+// import IdeaList from "./components/IdeaList";
+
+// const modal = new Modal();
+// const ideaForm = new IdeaForm();
+// ideaForm.render();
+
+// const ideaList = new IdeaList();
+// ideaList.render();
+import "@fortawesome/fontawesome-free/css/all.css";
+import Modal from "./components/Modal";
+import IdeaForm from "./components/IdeaForm";
+import IdeaList from "./components/IdeaList";
+import "./css/style.css";
+
+new Modal();
+const ideaForm = new IdeaForm();
+ideaForm.render();
+new IdeaList();
