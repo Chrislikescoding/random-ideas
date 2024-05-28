7940e749eeaf545f72d7ea8e587ae4f4b63c73cc
ChrisLikesCoding <sharpchr@gmail.com>
2024-05-28 21:08:03 +0100

    small changes!

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,4 @@
-// import "./css/style.css";
-// import Modal from "./components/Modal";
-// import IdeaForm from "./components/IdeaForm";
-// import "@fortawesome/fontawesome-free/css/all.css";
-// import IdeaList from "./components/IdeaList";
-
-// const modal = new Modal();
-// const ideaForm = new IdeaForm();
-// ideaForm.render();
-
-// const ideaList = new IdeaList();
-// ideaList.render();
-import "@fortawesome/fontawesome-free/css/all.css";
+//import "@fortawesome/fontawesome-free/css/all.css";
 import Modal from "./components/Modal";
 import IdeaForm from "./components/IdeaForm";
 import IdeaList from "./components/IdeaList";
